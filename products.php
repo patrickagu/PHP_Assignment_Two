19940c87633e62019b74b67c8be63eed526c27a0
patrickagu <63365045+patrickagu@users.noreply.github.com>
2025-08-08 23:07:42 -0400

    Update products.php

--- products.php
+++ products.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/header.php';
 
 $pageTitle = "Products - Grab & Go";
 $products = getProducts();
@@ -165,4 +166,5 @@ $sampleProducts = [
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
